2642b05eacbaa231500017f36f349202824dfe38
AgasnotPro <agasnotpro@gmail.com>
2021-06-15 17:37:08 +0200

    class PlaylistViewModel

--- app/src/main/java/com/trap/project_music/ui/main/home/PlaylistFragment.kt
+++ app/src/main/java/com/trap/project_music/ui/main/home/PlaylistFragment.kt
@@ -7,28 +7,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.trap.project_music.R
 import com.trap.project_music.ui.main.home.adapter.PlaylistAdapter
 import com.trap.project_music.ui.main.home.viewmodel.PlaylistViewModel
 
 class PlaylistFragment : Fragment() {
 
-    private lateinit var viewModel: PlaylistViewModel
     private lateinit var adapter: PlaylistAdapter
+    private val model: PlaylistViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.playlist_fragment, container, false)
-
-
-        adapter = PlaylistAdapter(listOf())
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PlaylistViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = PlaylistAdapter(listOf())
+
+        model.loadPlaylists()
+
+        //viewModel = ViewModelProvider(this).get(PlaylistViewModel::class.java)
         // TODO: Use the ViewModel
 
         Log.d("test", "PLAYLIST FRAGMENT")
