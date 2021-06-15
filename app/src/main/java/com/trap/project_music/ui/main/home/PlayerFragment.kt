78eac36ababa4b064eeccf0d0be68d97d769d6e1
William <william.andrieu@edu.itescia.fr>
2021-06-15 10:59:51 +0200

    Refactor player

--- app/src/main/java/com/trap/project_music/ui/main/home/PlayerFragment.kt
+++ app/src/main/java/com/trap/project_music/ui/main/home/PlayerFragment.kt
@@ -1,11 +1,9 @@
 package com.trap.project_music.ui.main.home
 
-import android.media.AudioManager
 import android.media.MediaPlayer
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -15,19 +13,13 @@ import android.widget.Toast
 import androidx.lifecycle.Observer
 import com.trap.project_music.ui.main.home.viewmodel.PlayerViewModel
 import com.trap.project_music.R
-import com.trap.project_music.factory.HomeViewModelFactory
 import com.trap.project_music.factory.PlayerViewModelFactory
 import com.trap.project_music.server.RetrofitFactory
-import com.trap.project_music.server.service.APIArtist
 import com.trap.project_music.server.service.APISong
-import com.trap.project_music.ui.main.home.viewmodel.HomeViewModel
-import com.trap.project_music.ui.main.home.viewmodel.HomeViewModelState
 import com.trap.project_music.ui.main.home.viewmodel.PlayerViewModelState
-import com.trap.project_music.vo.ArtistJSON
 import com.trap.project_music.vo.SongJSON
 import kotlinx.android.synthetic.main.player_fragment.*
 import kotlinx.coroutines.Runnable
-import java.lang.Exception
 
 class PlayerFragment : Fragment() {
 
@@ -36,7 +28,7 @@ class PlayerFragment : Fragment() {
     private var listSong = mutableListOf<SongJSON>()
 
     private lateinit var actualSong : SongJSON
-    private var actualSongIndex : Int = 0
+
 
     private lateinit var mediaPlayer: MediaPlayer
     private lateinit var runnable: Runnable
@@ -57,29 +49,23 @@ class PlayerFragment : Fragment() {
         viewModel = ViewModelProvider(this, PlayerViewModelFactory(RetrofitFactory(requireContext()).createService(APISong::class.java))).get(PlayerViewModel::class.java)
 
         viewModel.getStateListSong().observe(viewLifecycleOwner, Observer { updateUI(it) })
+
+        viewModel.getStateActualSong().observe(viewLifecycleOwner, Observer { updateActualSong(it) })
+
         viewModel.getSongs()
 
 
 
         play.setOnClickListener {
             this.stateMusic()
+
         }
 
         previous.setOnClickListener {
-            if (actualSongIndex > 0) {
-                actualSongIndex--
-                actualSong = listSong[actualSongIndex]
-                initalizeMediaPlayer()
-            }
+            viewModel.prevSong()
         }
         next.setOnClickListener {
-            if (actualSongIndex < listSong.size) {
-                actualSongIndex++
-                actualSong = listSong[actualSongIndex]
-                songName.text = actualSong.name
-                initalizeMediaPlayer()
-                stateMusic()
-            }
+            viewModel.nextSong()
         }
 
 
@@ -96,13 +82,27 @@ class PlayerFragment : Fragment() {
         })
     }
 
+    private fun updateActualSong(state: PlayerViewModelState?) {
+        when (state) {
+            is PlayerViewModelState.ChangeSong -> {
+                actualSong = state.actualSong
+                changeSong()
+                stateMusic()
+            }
+            is PlayerViewModelState.Failure -> {
+                println("Ca beug PlayerViewModelState updateActualSong")
+            }
+            is PlayerViewModelState.Loading -> {
+                println("Loading")
+            }
+        }
+
+    }
+
     private fun updateUI(state: PlayerViewModelState?) {
         when (state) {
-            is PlayerViewModelState.Succes -> {
+            is PlayerViewModelState.Success -> {
                 this.listSong.addAll(state.listSong)
-                this.actualSong = this.listSong[0]
-                songName.text = actualSong.name
-                this.initalizeMediaPlayer()
             }
             is PlayerViewModelState.Failure -> {
                 println("Ca beug PlayerViewModelState")
@@ -113,21 +113,23 @@ class PlayerFragment : Fragment() {
         }
 
     }
-
-    private fun initalizeMediaPlayer(){
+    private fun changeSong() {
+        if (this::runnable.isInitialized) handler.removeCallbacks(runnable)
+        songName.text = actualSong.name
         mediaPlayer.reset()
         mediaPlayer.setDataSource(actualSong.file)
         mediaPlayer.prepare()
         this.initializeSeekBar()
+        handler.postDelayed(runnable, 1000)
     }
+
 
     private fun initializeSeekBar() {
         seekBar.max = mediaPlayer.duration
         runnable = Runnable {
             seekBar.progress = mediaPlayer.currentPosition
-           // handler.postDelayed(runnable, 1000)
+            handler.postDelayed(runnable, 1000)
         }
-        handler.postDelayed(runnable, 1000)
     }
 
     private fun stateMusic() {
--- app/src/main/java/com/trap/project_music/ui/main/home/viewmodel/PlayerViewModel.kt
+++ app/src/main/java/com/trap/project_music/ui/main/home/viewmodel/PlayerViewModel.kt
@@ -1,13 +1,13 @@
 package com.trap.project_music.ui.main.home.viewmodel
 
+import android.media.MediaPlayer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.trap.project_music.server.service.APIArtist
 import com.trap.project_music.server.service.APISong
-import com.trap.project_music.vo.ArtistJSON
 import com.trap.project_music.vo.SongJSON
+import kotlinx.android.synthetic.main.player_fragment.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,36 +15,49 @@ import retrofit2.Response
 
 sealed class PlayerViewModelState() {
 
-    data class Succes(val listSong: List<SongJSON>) : PlayerViewModelState()
+    data class Success(val listSong: List<SongJSON>) : PlayerViewModelState()
     data class Loading(val message: String) : PlayerViewModelState()
     data class Failure(val errorMessage: String) : PlayerViewModelState()
+
+    data class ChangeSong(val actualSong: SongJSON) : PlayerViewModelState()
 
 }
 
 class PlayerViewModel(private val apiSong: APISong) : ViewModel() {
 
-
     private val state = MutableLiveData<PlayerViewModelState>()
 
+    private var listSong = mutableListOf<SongJSON>()
 
-    private lateinit var actualSong : SongJSON
-    private  var actualSongIndex : Int = 0
+    private var actualSongIndex: Int = 0
     private val stateListSong = MutableLiveData<PlayerViewModelState>()
+
+    private val actualSong = MutableLiveData<PlayerViewModelState>()
 
     fun getState(): LiveData<PlayerViewModelState> = state
     fun getStateListSong(): LiveData<PlayerViewModelState> = stateListSong
+    fun getStateActualSong(): LiveData<PlayerViewModelState> = actualSong
 
 
-
-    fun nextSong(){
-        if (actualSongIndex < 10){
-        actualSongIndex++
-        actualSong 
+    fun nextSong() {
+        if (actualSongIndex < listSong.size) {
+            actualSongIndex += 1
+            changeActualSong(actualSongIndex)
         }
     }
-    fun prevSong(){
-        actualSong
+
+    fun prevSong() {
+        if (actualSongIndex > 0) {
+            actualSongIndex -= 1
+            changeActualSong(actualSongIndex)
+        }
     }
+
+
+    fun changeActualSong(index: Int) {
+        actualSong.value = PlayerViewModelState.ChangeSong(listSong[index])
+    }
+
 
     fun getSongs() {
         stateListSong.value = PlayerViewModelState.Loading("Chargement : ")
@@ -56,14 +69,16 @@ class PlayerViewModel(private val apiSong: APISong) : ViewModel() {
                 stateListSong.value = PlayerViewModelState.Failure("Error")
             }
 
-            override fun onResponse(call: Call<List<SongJSON>>, response: Response<List<SongJSON>>) {
+            override fun onResponse(
+                call: Call<List<SongJSON>>,
+                response: Response<List<SongJSON>>
+            ) {
                 response.body()?.also { it ->
                     Log.d("RESPONSE", "SONG list $it")
 
-                    val listSong : List<SongJSON> = it
-                    actualSong = listSong[actualSongIndex]
-
-                    stateListSong.value = PlayerViewModelState.Succes(listSong)
+                    listSong.addAll(it)
+                    changeActualSong(actualSongIndex)
+                    stateListSong.value = PlayerViewModelState.Success(listSong)
                 } ?: run {
                     stateListSong.value = PlayerViewModelState.Failure("list null")
                 }
